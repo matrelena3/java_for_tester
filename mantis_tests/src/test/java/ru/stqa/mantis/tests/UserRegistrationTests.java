0ae55cc8fa8f6d55024b97b04a8d0f925c07396b
Elena <diamond_girl_89@inbox.ru>
2025-12-17 20:49:04 +0300

    8.6

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.mantis.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTests extends TestBase {
+
+    @Test
+    void canRegisterUser(String username) {
+        var email = String.format("%s@localhost", username);
+
+    }
+}
